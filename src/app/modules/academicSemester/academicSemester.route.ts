0247df924b4e217ef977095bae0c4bd179155ea8
Jahedul Hoque <jahedulh1@gmail.com>
2023-12-02 16:27:12 +0100

    Academic semester updated

--- src/app/modules/academicSemester/academicSemester.route.ts
+++ src/app/modules/academicSemester/academicSemester.route.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { ENUM_USER_ROLE } from '../../../enums/user';
 import auth from '../../middlewares/auth';
-import validateRequest from '../../middlewares/validateRequest';
 import { AcademicSemesterController } from './academicSemester.controller';
 
 const router = express.Router();
--- src/app/modules/academicSemester/academicSemester.service.ts
+++ src/app/modules/academicSemester/academicSemester.service.ts
@@ -1,60 +1,63 @@
-import { Request } from "express"
-import { CoreService as HttpService } from "../../../shared/axios"
-import { IGenericErrorResponse, IGenericResponse } from "../../../interfaces/common";
+import { Request } from 'express';
+import { IGenericResponse } from '../../../interfaces/common';
+import { CoreService as HttpService } from '../../../shared/axios';
 
-const create = async (req: Request):Promise<IGenericResponse> => {
-    const response: IGenericResponse = await HttpService.post('/academic-semesters', req.body, {
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
-}
+const create = async (req: Request): Promise<IGenericResponse> => {
+  const response: IGenericResponse = await HttpService.post('/academic-semesters', req.body, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
 
-const findAll = async (req: Request):Promise<IGenericResponse> => {
-    const response: IGenericResponse = await HttpService.get('/academic-semesters', {
-        params: req.query,
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
-}
+const findAll = async (req: Request): Promise<IGenericResponse> => {
+  const response: IGenericResponse = await HttpService.get('/academic-semesters', {
+    params: req.query,
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
 
-const findOne = async (req: Request):Promise<IGenericResponse> => {
-    const {id} = req.params;
-    const response: IGenericResponse = await HttpService.get(`/academic-semesters/${id}`, {
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
-}
+const findOne = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  const response: IGenericResponse = await HttpService.get(`/academic-semesters/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
 
-const update = async (req: Request):Promise<IGenericResponse> => {
-    const response: IGenericResponse = await HttpService.patch(`/academic-semesters/${req.params.id}`, req.body, {
-        params: req.query,
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
-}
+const update = async (req: Request): Promise<IGenericResponse> => {
+  const response: IGenericResponse = await HttpService.patch(
+    `/academic-semesters/${req.params.id}`,
+    req.body,
+    {
+      headers: {
+        Authorization: req.headers.authorization
+      }
+    }
+  );
+  return response;
+};
 
-const deleteOne = async (req: Request):Promise<IGenericResponse> => {
-    const {id} = req.params;
-    const response: IGenericResponse = await HttpService.delete(`/academic-semesters/${id}`, {
-        headers: {
-            Authorization: req.headers.authorization
-        }
-    });
-    return response;
-}
+const deleteOne = async (req: Request): Promise<IGenericResponse> => {
+  const { id } = req.params;
+  const response: IGenericResponse = await HttpService.delete(`/academic-semesters/${id}`, {
+    headers: {
+      Authorization: req.headers.authorization
+    }
+  });
+  return response;
+};
 
 export const AcademicSemesterService = {
-    create,
-    findAll,
-    findOne,
-    update,
-    deleteOne
-}
+  create,
+  findAll,
+  findOne,
+  update,
+  deleteOne
+};
